2b8f5a041d71df8bb292a3185034c6998207f713
Dmitriy <93647407+1swaN@users.noreply.github.com>
2023-07-23 22:26:39 +0400

    My basic gulp-package
    I have to add some npm packages:
    gulp-webp
    gulp-webp-html
    gulp-webp-css
    + add readme later (tomorrow or later)

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import hello from './modules/hello.js'
+
+// npm пакет для подключения кастомного калькулятора на js и нативном css - air-datepicker
+import AirDatepicker from 'air-datepicker'
+import 'air-datepicker/air-datepicker.css'
+
+new AirDatepicker('#date')
+//
+
+
+console.log(hello)
+
